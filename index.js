c1b550c5b46cb83d20d8041bcd51b69ef212a860
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-05 17:13:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-function happyHolidays() {
+function happyHolidays (){
   return "Happy holidays!";
 }
-
-function happyHolidaysTo(String) {
-  var name = 'you';
- return `Happy holidays, ${name}!`;
+   function happyHolidaysTo(name){
+  return   `Happy holidays, ${name}!`;
+  }
+  
+function  happyHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`;
 }
 
-function happyHolidayTo(holiday,name) {
-  var name = 'you';
-  var holiday = 'Independence Day';
- return `Happy ${holiday}, ${name}!`;
-}
-
-function holidayCountdown(holiday,days) {
-  var days = '20';
-  var holiday = "Mother's Day";
- return `It's ${days} days until ${holiday}!`;
+function holidayCountdown(holiday,days){
+  return `It\'s ${days} days until ${holiday}!`;
 }
