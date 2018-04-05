4d87f1efa36f8113c48d12b1b9cf83aef4fe4582
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-05 17:05:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function happyHolidayTo(holiday,name) {
 function holidayCountdown(holiday,name) {
   var name = 'you';
   var holiday = 'Independence Day';
- return `It's ${days}, ${name}!`;
+ return `It's ${days} days until ${holiday}!`;
 }
